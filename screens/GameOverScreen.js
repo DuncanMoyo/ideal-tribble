94b6bc7c9676ab608c75d2f3eaadcafe29601350
Duncan Moyo <duncanfmoyo@gmail.com>
2021-04-06 23:47:54 +0200

    working with network (web) images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -7,8 +7,16 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over</TitleText>
-      <View style={styles.imageContainer} >
-        <Image source={require("../assets/success.png")} style={styles.image} />
+      <View style={styles.imageContainer}>
+        {/* <Image source={require("../assets/success.png")} style={styles.image} /> */}
+        <Image
+          fadeDuration={1000}
+          source={{
+            uri:
+              "https://s3.amazonaws.com/images.gearjunkie.com/uploads/2018/05/matterhorn-3x2.jpg",
+          }}
+          style={styles.image}
+        />
       </View>
       <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number Was: {props.userNumber}</BodyText>
@@ -26,15 +34,15 @@ const styles = StyleSheet.create({
   imageContainer: {
     borderRadius: 150,
     borderWidth: 3,
-    borderColor: 'black',
+    borderColor: "black",
     width: 300,
     height: 300,
-    overflow: 'hidden',
-    marginVertical: 30
+    overflow: "hidden",
+    marginVertical: 30,
   },
   image: {
     width: "100%",
-    height: '100%',
+    height: "100%",
   },
 });
 
